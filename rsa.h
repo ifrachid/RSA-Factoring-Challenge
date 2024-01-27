4cb6cdfa5d44121935cf773fad724082fcaf48a7
rachid <rachid.iflou@gmail.com>
2024-01-27 08:03:17 -0800

    update

--- rsa.h
+++ rsa.h
@@ -1,29 +1,20 @@
-#ifndef _RSA_H_
-#define _RSA_H_
+#!/usr/bin/env bash 
+# Factorize as many numbers as possible into a product of two smaller numbers.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <inttypes.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define S_BUFFER 2000
-#define VALUE 1000
-
-/* factors.c */
-void init(char *buffer);
-void file2buffer(char *filename, char *buffer);
-long long unsigned int _atoi(char *src, long long unsigned int *dest);
-void _putint(long long unsigned int *dest, long long unsigned int n);
-
-/* pollard-rho.c */
-long long unsigned int modular_pow(long long unsigned int base, int exponent, long long unsigned int modulus);
-long long unsigned int __gcd(long long unsigned int a, long long unsigned int b);
-long long unsigned int PollardRho(long long unsigned int n);
-long long unsigned int compute(long long unsigned int *src, long long unsigned int n);
-
-#endif /* _RSA_H */
+while IFS= read -r LINE
+do
+    let FLAG=1
+    let DIV=2
+    while [ $FLAG -eq 1 ]
+    do
+	REST=$(($LINE%$DIV))
+	if [[ $REST -eq 0 ]]
+        then
+            let NUM=$LINE
+            let COUNT=$(($NUM/$DIV))
+            echo "$LINE=$COUNT*$DIV"
+            let FLAG=0
+        fi
+        let DIV=$(($DIV+1))
+    done
+done < $1
